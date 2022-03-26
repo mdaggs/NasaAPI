f1f64c715d40f0d2980df6a68dd2526b75771f7c
mdlumen <matthew.daggs@lumen.com>
2022-03-26 10:48:16 -0500

    Cleanup of old code. Specified a new Save Folder.

--- Photo Of the Day Endpoint.py
+++ Photo Of the Day Endpoint.py
@@ -9,42 +9,41 @@ from config import api_key, executable_path
 
 def nasaphoto():
 
-	#define variables
+	#----- Define variables -----
 	endpoint = r"https://api.nasa.gov/planetary/apod?api_key={}".format(api_key) 
 
-	#grab data
+	#----- Grab data -----
 	data = requests.get(endpoint)
-	#convert to JSON
+
+	#----- Convert to JSON -----
 	textdata = json.loads(data.text)
-	#grab data out of JSON
+
+	#----- Grab data out of JSON -----
 	newurl = textdata["url"]
 	explanation = textdata["explanation"]
-	#photographer = textdata["copyright"]
 	date = textdata["date"]
 	title = textdata["title"]
 
-	#Print title, data, photographer and explanation
+	#----- Print title, data, photographer and explanation -----
 	print(title)
 	print("\n")
+
 	print(date)
 	print("\n")
-	#print(photographer)
+
 	print("\n")
 	print(explanation)
 
-	#download the photo of the day
+	#----- Download The Photo Of The Day -----
 	img_data = requests.get(newurl).content
-	with open(date + ' ' + 'NasaPhotoOfTheDay.jpg', 'wb') as handler:
+
+	#----- Specify Save Locaiton -----
+	hierFolder = 'NASAPhotos/'
+
+	#----- Save Photo -----
+	with open(hierFolder + date + ' ' + 'NasaPhotoOfTheDay.jpg', 'wb') as handler:
 		handler.write(img_data)
-
-	#visit url in chrome webdriver
-	# Create a new instance of the browser, make sure we can see it (Headless = False)
-	# browser = Browser('chrome', **executable_path, headless=False)
-	# browser.visit(newurl)
-	# time.sleep(20)
-	# browser.quit()
-
-	# print("\nCode Has Finished Running")
+		print(handler)
 
 
 nasaphoto()
